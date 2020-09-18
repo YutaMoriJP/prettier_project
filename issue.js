da676d4ac7a529d7df0e75de1d2e056d36a4b8c8
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2020-09-18 07:25:08 -0700

    third commit

--- issue.js
+++ issue.js
@@ -1,6 +1,13 @@
-class Profile {constructor(name){this.name=name};intro(){return this.name}};
+class Profile {
+  constructor(name) {
+    this.name = name;
+  }
+  intro() {
+    return this.name;
+  }
+}
 
-const p1 = new Profile('Yuta');
+const p1 = new Profile("Yuta");
 
-p1.__proto__ === Profile.prototype
+p1.__proto__ === Profile.prototype;
 p1.intro();
