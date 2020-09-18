23fde3af495cc711e084a9e5dfa7094c2f6e2f63
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2020-09-18 07:16:58 -0700

    fixed js file

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const o={birthday: '1995 02 14',get age(){return new Date().getFullYear() - new Date(this.birthday).getFullYear()}};
+const o = {
+  birthday: "1995 02 14",
+  get age() {
+    return new Date().getFullYear() - new Date(this.birthday).getFullYear();
+  },
+};
 
-const bio = ({birthday,age}) =>`My birthday is ${birthday} and my age is ${age}`;
+const bio = ({ birthday, age }) =>
+  `My birthday is ${birthday} and my age is ${age}`;
